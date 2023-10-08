c408d0b9e2c783a02ad47102796990e7cf578961
Erik Marsh <erik.i.marsh@gmail.com>
2023-10-07 20:20:19 -0700

    distributions seem to be very light, no need to carry them in the context

--- RandomContext.hpp
+++ RandomContext.hpp
@@ -6,6 +6,4 @@ struct RandomContext
 {
     std::random_device::result_type seed;
     std::mt19937 gen;
-    std::uniform_int_distribution<int> distInts;
-    std::normal_distribution<double> distGauss;
 };
